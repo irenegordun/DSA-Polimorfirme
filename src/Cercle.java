34abb9261856c0b276f6b0da6b21a3941a084c75
esmarcuar <esmarcuar@gmail.com>
2022-03-08 18:17:26 +0100

    segundo intento cercle

--- src/Cercle.java
+++ src/Cercle.java
@@ -1,3 +1,10 @@
-public class Cercle {
-
+public class Cercle extends Figura{
+    private double radi;
+    private static double PI=3.1415;
+    Cercle (double radi){
+        this.radi=radi;
+    }
+    public double area (){
+       return PI*radi*radi;
+    }
 }
