9cfa5395ed4a53ecb0e45d21eaa20fce527b04a4
esmarcuar <esmarcuar@gmail.com>
2022-03-08 18:59:06 +0100

    rectangle i quadrat

--- src/Quadrat.java
+++ src/Quadrat.java
@@ -1,2 +1,6 @@
-public class Quadrat {
+public class Quadrat extends Rectangle{
+    Quadrat(double costat){
+        super(costat,costat);
+    }
+
 }
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,13 @@
-public class Rectangle {
+public class Rectangle extends Figura {
+    private double costat1;
+    private double costat2;
+
+    Rectangle(double costat1, double costat2){
+        this.costat1=costat1;
+        this.costat2=costat2;
+    }
+
+    public double area(){
+        return costat1*costat2;
+    }
 }
