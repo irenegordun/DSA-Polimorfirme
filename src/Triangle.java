fc0e73d00f08b89380b72dff9ae87c12748ebb6d
esmarcuar <esmarcuar@gmail.com>
2022-03-08 18:47:28 +0100

    Triangle

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,2 +1,13 @@
-public class Triangle {
+public class Triangle extends Figura{
+    private double b;
+    private double h;
+
+    Triangle(double b,double h){
+        this.b=b;
+        this.h=h;
+    }
+
+    public double area(){
+        return b*h/2;
+    }
 }
