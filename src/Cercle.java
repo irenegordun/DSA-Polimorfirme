74af2255e3411bdb3616260ed8f17832588b39fc
Alba Onrubia <alba.onrubia@estudiantat.upc.edu>
2022-03-08 18:35:34 +0100

    Con espacios

--- src/Cercle.java
+++ src/Cercle.java
@@ -1,10 +1,12 @@
 public class Cercle extends Figura{
     private double radi;
     private static double PI=3.1415;
+
     Cercle (double radi){
         this.radi=radi;
     }
-    public double area (){
+
+    public double area () {
        return PI*radi*radi;
     }
 }
